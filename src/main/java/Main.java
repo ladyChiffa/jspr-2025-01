19e4b037df68f880863c98bac0516fd31a45a5e8
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2025-10-14 20:20:38 +0300

    Add handlers behaviour

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,36 @@
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         final var server = new Server(64);
+
+        server.addHandler("GET", "/classic.html", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var path = request.getAction();
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                ).getBytes();
+                responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                ).getBytes());
+                responseStream.write(content);
+                responseStream.flush();
+            }
+        });
+
         server.start(9999);
     }
 }
---
+++ src/main/java/Request.java
@@ -0,0 +1,122 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Request {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+
+    protected String method;
+    protected String action;
+    protected String parameters;
+    protected List<String> headers;
+    protected String body;
+    protected String errorDescription = null;
+    public Request (BufferedInputStream in) throws IOException {
+        final var allowedMethods = List.of(GET, POST);
+        final var limit = 4096;
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            errorDescription = "Некорректная или пустая строка запроса";
+            return;
+        }
+
+        String requestLine = new String(Arrays.copyOf(buffer, requestLineEnd));
+
+        if(requestLine == null) {
+            errorDescription = "Пустая строка запроса";
+            return;
+        }
+
+        String[] parts = requestLine.split(" ");
+        if(parts.length != 3) {
+            errorDescription = "Некорректная строка запроса";
+            return;
+        }
+
+        method = parts[0];
+        if (!allowedMethods.contains(method)) {
+            errorDescription = "Недопустимый метод";
+            return;
+        }
+
+        String[] fullaction = parts[1].split("\\?");
+        action = fullaction[0];
+        parameters = fullaction.length > 1 ? fullaction[1] : "";
+
+        if (!action.startsWith("/")) {
+            errorDescription = "Некорректное имя ресурса";
+            return;
+        }
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            errorDescription = "Ошибка при выборе заголовков";
+            return;
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+        // для GET тела нет
+        if (!method.equals(GET)) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers, "Content-Length");
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+
+                body = new String(bodyBytes);
+                System.out.println(body);
+            }
+        }
+
+    }
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    public boolean isGoodRequest() {
+        return errorDescription == null;
+    }
+    public String requireHandler (){
+        return method + "," + action;
+    }
+    public String getAction(){
+        return action;
+    }
+}
